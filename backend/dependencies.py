9a28928c9689e0ea467496567e6d440b87fdddf5
Shrey Jain <jainshreyp@gmail.com>
2025-11-09 10:12:07 -0800

    yo

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import Annotated
 
-from models.user import AuthenticatedUser
+from schemas.user import AuthenticatedUser
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
